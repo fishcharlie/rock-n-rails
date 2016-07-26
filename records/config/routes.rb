e08fe6ba01cfa1bf2fa614130c58333ad55db2ba
Charlie Fish <fishcharlie@me.com>
2016-07-26 15:15:33 -0600

    Fixing typo

--- records/config/routes.rb
+++ records/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :records, expect: [:delete, :edit, :update]
+  resources :records, except: [:delete, :edit, :update]
 end
